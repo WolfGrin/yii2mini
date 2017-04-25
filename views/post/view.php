4e6ac2407b8b8f28781cfef303f8214716110793
Dmitriy Mikhaylik <sblazze@gmail.com>
2017-04-25 11:08:49 +0300

    Вывод материала на страницу. Исключение - 404 ошибка

---
+++ views/post/view.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 25.04.2017
+ * Time: 10:39
+ */
+?>
+
+<div class="panel panel-default">
+    <div class="panel-heading">
+        <h3 class="panel-title"><?= $post->title ?></h3>
+</div>
+<div class="panel-body">
+    <?= $post->text ?>
+</div>
+</div>
